c9a4323a829b109b7927136c3128b9feb87f33e0
RWDevelopment <33756545+RWDevelopment@users.noreply.github.com>
2018-07-12 20:22:13 +0200

    Update ticker.js

--- assets/js/ticker.js
+++ assets/js/ticker.js
@@ -1,18 +1,10 @@
 $(function(){
 	$(".typed").typed({
-		strings: ["<h1>The Spanish Inquisition</h2>^1000\n", "<h2>is looking for</h2>", "People."],
+		strings: ["<h1>The Spanish Inquisition</h2>^1000\n", "<h2>is looking for</h2>^1000\n", "<h2>gifted & mentally ill</h2>"],
 		// typing speed
-		typeSpeed: 30,
+		typeSpeed: 50,
 		// time before typing starts
 		startDelay: 1200,
-		// backspacing speed
-		backSpeed: 20,
-		// time before backspacing
-		backDelay: 500,
-		// loop
-		loop: true,
-		// false = infinite
-		loopCount: 5,
 		// show cursor
 		showCursor: true,
 		// character for cursor
