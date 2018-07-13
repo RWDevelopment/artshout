607f37d55939694743a0a845507a2665af48f804
RWDevelopment <33756545+RWDevelopment@users.noreply.github.com>
2018-07-13 12:23:02 +0200

    Update ticker.js

--- assets/js/ticker.js
+++ assets/js/ticker.js
@@ -2,5 +2,6 @@ var typed = new Typed('.typewriter', {
     strings: ['npm install^1000\n `installing components...` ^1000\n `Fetching from source...`'],
     typeSpeed: 40,
     backSpeed: 0,
+    cursorChar: '_',
     loop: true
 });
