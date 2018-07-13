f017739995b67c03a95c13c7c7effc9bd56171a4
RWDevelopment <33756545+RWDevelopment@users.noreply.github.com>
2018-07-13 13:37:01 +0200

    Update ticker.js

--- assets/js/ticker.js
+++ assets/js/ticker.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typewriter', {
-    strings: ['`&nbsp;...&nbsp;`^2000\n The Spanish Inquisition^2000\n `&spades;`\n is looking for^1000\n <i>gifted and mentally ill</i>^1000\n <u>graphic designer</u>^1000\n as a team member^1000\n <del>if You are brave enough</del>^1000\n drop Us a line^1000\n email ......^3000'],
+    strings: ['`&nbsp;------------------------------&nbsp;`^2000\n The Spanish Inquisition^2000\n `&spades;`\n is looking for^1000\n <i>gifted and mentally ill</i>^1000\n <u>graphic designer</u>^1000\n as a team member^1000\n <del>if You are brave enough</del>^1000\n drop Us a line^1000\n email ......^3000'],
     typeSpeed: 50,
     backSpeed: 0,
     cursorChar: ' &commat;',
