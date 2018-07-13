05d13746996a476aa426271a0ddee1a89df3c293
RWDevelopment <33756545+RWDevelopment@users.noreply.github.com>
2018-07-13 12:43:31 +0200

    Update ticker.js

--- assets/js/ticker.js
+++ assets/js/ticker.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typewriter', {
-    strings: ['The Spanish Inquisition^1000\n `is looking for`^1000\n `Fetching from source...`'],
+    strings: ['The Spanish Inquisition^1000\n `is looking for`^1000\n `gifted and mentally ill`^1000\n `graphic designer`^1000'],
     typeSpeed: 40,
     backSpeed: 0,
     cursorChar: '_',
